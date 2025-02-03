efeb9a2f7235ba4b91617d786ada92c1fd736e7b
Francisco Marchi <fmarchi500@gmail.com>
2025-02-03 11:26:35 -0500

    Update package metadata: change author, license, and add classifiers

--- setup.py
+++ setup.py
@@ -5,31 +5,25 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     package_data={
-        'alma_classifier': ['models/__init__.py'],
+        'alma_classifier': ['models/*', 'data/*'],
     },
-    install_requires=[
-        "pandas~=2.0.3",
-        "numpy~=1.24.4",
-        "scikit-learn~=1.2.2",
-        "lightgbm~=3.3.5",
-        "joblib~=1.3.2",
-        "openpyxl>=3.0.0"  # For Excel support
-    ],
+    install_requires=[],  # Dependencies are managed in pyproject.toml
     entry_points={
         'console_scripts': [
             'alma-classifier=alma_classifier.cli:main',
         ],
     },
-    python_requires='>=3.8',
-    author="Your Name",
+    python_requires='==3.8',
+    author="Francisco Marchi",
+    author_email="fmarchi500@gmail.com",
     description="A Python package for applying pre-trained epigenomic classification models",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/alma-classifier",
+    url="https://https://github.com/f-marchi/ALMA-classifier",
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
 )
